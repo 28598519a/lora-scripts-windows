d360df1e1839c14097348d73a0679d8487e1c5a2
劃破黑夜 <179971b@gmail.com>
2023-03-05 02:18:28 +0800

    Update train.ps1

--- train.ps1
+++ train.ps1
@@ -1,40 +1,40 @@
 ﻿# Train data path | 設置訓練用模型、圖片
-$pretrained_model = "./sd-models/model.ckpt" # base model path | 底模路徑 (pruned或full都行，不要vae)
-$train_data_dir = "./train/Graf" # train dataset path | 訓練數據集路徑
-$reg_data_dir = "" # directory for regularization images | 正則化數據集路徑，默認不使用正則化圖像。
+$pretrained_model = "./sd-models/model.ckpt" # base model path | 底模路徑 (pruned或full都行，不要使用vae)
+$train_data_dir = "./train/Graf"             # train dataset path | 訓練數據集路徑
+$reg_data_dir = ""                           # directory for regularization images | 正則化數據集路徑，默認不使用正則化圖像。
 
 # Train related params | 訓練相關參數
-$resolution = "512,512" # image resolution w,h. 圖片分辨率，寬,高。支持非正方形，但必須是 64 倍數。
-$batch_size = 1 # batch size
-$max_train_epoches = 20 # max train epoches | 最大訓練 epoch
-$save_every_n_epochs = 2 # save every n epochs | 每 N 個 epoch 保存一次
-$network_dim = 32 # network dim | 常用 4~128，不是越大越好
-$network_alpha = 16 # network alpha | 常用與 network_dim 相同的值或者採用較小的值，如 network_dim的一半 防止下溢。默認值為 1，使用較小的 alpha 需要提升學習率。
-$clip_skip = 2 # clip skip | 一般Anime用 2 (因為NAI)
-$train_unet_only = 0 # train U-Net only | 僅訓練 U-Net，開啟這個會犧牲效果大幅減少顯存使用。6G顯存可以開啟
+$resolution = "512,512"      # image resolution w,h. 圖片分辨率，寬,高。支持非正方形，但必須是 64 倍數。
+$batch_size = 1              # batch size
+$max_train_epoches = 20      # max train epoches | 最大訓練 epoch
+$save_every_n_epochs = 2     # save every n epochs | 每 N 個 epoch 保存一次
+$network_dim = 32            # network dim | 常用 4~128，不是越大越好
+$network_alpha = 16          # network alpha | 常用與 network_dim 相同的值或者採用較小的值，如 network_dim的一半 防止下溢。默認值為 1，使用較小的 alpha 需要提升學習率。
+$clip_skip = 2               # clip skip | 一般Anime用 2 (因為NAI)
+$train_unet_only = 0         # train U-Net only | 僅訓練 U-Net，開啟這個會犧牲效果大幅減少顯存使用。6G顯存可以開啟
 $train_text_encoder_only = 0 # train Text Encoder only | 僅訓練 文本編碼器
 
 # Learning rate | 學習率
 $lr = "1e-4"
 $unet_lr = "1e-4"
 $text_encoder_lr = "1e-5"
-$lr_scheduler = "cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"
-$lr_warmup_steps = 0 # warmup steps | 僅在 lr_scheduler 為 constant_with_warmup 時需要填寫這個值
-$lr_restart_cycles = 1 # cosine_with_restarts restart cycles | 餘弦退火重啟次數，僅在 lr_scheduler 為 cosine_with_restarts 時起效。
+$lr_scheduler = "cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup" | 學習率動態調整方式
+$lr_warmup_steps = 0                   # warmup steps | 僅在 lr_scheduler 為 constant_with_warmup 時需要填寫這個值
+$lr_restart_cycles = 1                 # cosine_with_restarts restart cycles | cosine調整重複次數，僅在 lr_scheduler 為 cosine_with_restarts 時起效。
 
 # Output settings | 輸出設置
-$output_name = "Graf" # output model name | 模型保存名稱
+$output_name = "Graf"          # output model name | 模型保存名稱
 $save_model_as = "safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
 
 # 其他設置
-$network_weights = "" # pretrained weights for LoRA network | 若需要從已有的 LoRA 模型上繼續訓練，請填寫 LoRA 模型路徑。
-$min_bucket_reso = 256 # arb min resolution | arb 最小分辨率
-$max_bucket_reso = 1024 # arb max resolution | arb 最大分辨率
+$network_weights = ""               # pretrained weights for LoRA network | 若需要從已有的 LoRA 模型上繼續訓練，請填寫 LoRA 模型路徑。
+$min_bucket_reso = 256              # arb min resolution | arb 最小分辨率
+$max_bucket_reso = 1024             # arb max resolution | arb 最大分辨率
 $persistent_data_loader_workers = 0 # persistent dataloader workers | 容易爆內存，保留加載訓練集的worker，減少每個 epoch 之間的停頓
 
 # 優化器設置
 $use_8bit_adam = 1 # use 8bit adam optimizer | 使用 8bit adam 優化器節省顯存，默認啟用。部分 10 系老顯卡無法使用，修改為 0 禁用。
-$use_lion = 0 # use lion optimizer | 使用 Lion 優化器
+$use_lion = 0      # use lion optimizer | 使用 Lion 優化器
 
 
 # ============= DO NOT MODIFY CONTENTS BELOW | 請勿修改下方內容 =====================
